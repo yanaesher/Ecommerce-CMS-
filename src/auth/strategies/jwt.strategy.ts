18f37e13a06382c1a51b2668ac92b6ca38260825
yanaesher <senyukyana26@gmail.com>
2025-10-02 22:03:12 +0200

    jwt module

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -7,7 +7,7 @@ import { PassportStrategy } from '@nestjs/passport'
 import { User } from '@prisma/client'
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 	constructor(
 		private configService: ConfigService,
 		private userService: UserService
